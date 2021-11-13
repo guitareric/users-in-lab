cdcac65dee585f7d60149611a4a76f55ffeb7b5b
Eric Fluckiger <ericfluckiger@yahoo.com>
2021-11-13 16:31:02 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg'
-import './App.css'
+import MainPage from './pages/Main/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <MainPage />
+    </>
   )
 }
 
---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../../components/Container'
+
+function MainPage() {
+  return (
+    <Container>
+      <div>
+        <header className="text-primary text-center">Users in Lab</header>
+      </div>
+    </Container>
+  )
+}
+
+export default MainPage
