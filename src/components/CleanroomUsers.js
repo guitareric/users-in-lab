daee74284e982ccde2b56b10e6984395fff57c1b
Eric Fluckiger <ericfluckiger@yahoo.com>
2021-11-14 10:27:59 -0700

    updates

--- src/components/CleanroomUsers.js
+++ src/components/CleanroomUsers.js
@@ -1,29 +1,32 @@
 import React from 'react'
 import Header from './Header'
+import { usersInLab } from '../../src/data/mockData'
 
 export default function CleanroomUsers() {
+  let userInLab = usersInLab.map(element => <UserList name={element.name} time={element.time} location={element.location} />)
   return (
     <>
       <Header title="cleanroom" size="sm" />
       <table className="table table-hover table-dark">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Mark</td>
-          </tr>
-          <tr>
-            <td>Jacob</td>
-          </tr>
-          <tr>
-            <td colspan="2">Larry the Bird</td>
-          </tr>
-        </tbody>
+        <tbody>{userInLab}</tbody>
       </table>
+    </>
+  )
+}
+
+function UserList(props) {
+  const name = props.name
+  const time = props.time
+  const location = props.location
+  return (
+    <>
+      <tr>
+        <td className="text-capitalize font-weight-bold">
+          <h4>
+            {name} {time}
+          </h4>
+        </td>
+      </tr>
     </>
   )
 }
